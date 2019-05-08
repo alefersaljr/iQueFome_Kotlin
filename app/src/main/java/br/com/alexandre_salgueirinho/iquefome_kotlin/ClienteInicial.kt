f53db9f970653725febece6a6380e61b50e3e0b4
alefersaljr <a.salgueirinho@cinq.com.br>
2019-05-08 14:54:20 -0300

    08/05/2019
    - Exclusão do menu hamburguer
    - Inclusão da transição entre activities
    - Inicio da implementação do back button

--- app/src/main/java/br/com/alexandre_salgueirinho/iquefome_kotlin/ClienteInicial.kt
+++ app/src/main/java/br/com/alexandre_salgueirinho/iquefome_kotlin/ClienteInicial.kt
@@ -1,7 +1,9 @@
 package br.com.alexandre_salgueirinho.iquefome_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -12,31 +14,41 @@ class ClienteInicial : AppCompatActivity() {
     private lateinit var mDrawerLayout: DrawerLayout
     private lateinit var mToggle: ActionBarDrawerToggle
 
+    private lateinit var mToolbar: androidx.appcompat.widget.Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cliente_inicial)
 
-        mDrawerLayout = findViewById(R.id.drawer)
-        mToggle = ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close)
-        mDrawerLayout.addDrawerListener(mToggle)
-        mToggle.syncState()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mToolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(mToolbar)
+
+//        mDrawerLayout = findViewById(R.id.drawer)
+//        mToggle = ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close)
+//        mDrawerLayout.addDrawerListener(mToggle)
+//        mToggle.syncState()
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+//    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+//
+//        if (mToggle.onOptionsItemSelected(item)) {
+//            return true
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_menu, menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-
-        if (mToggle.onOptionsItemSelected(item)) {
-            when (item?.itemId){
-                R.id.menu_carrinho -> Toast.makeText(this, "Clicou em Carrinho", Toast.LENGTH_SHORT).show()
-                R.id.menu_historico -> Toast.makeText(this, "Clicou em Historico", Toast.LENGTH_SHORT).show()
-                R.id.menu_inicial -> Toast.makeText(this, "Clicou em Inicial", Toast.LENGTH_SHORT).show()
-                R.id.menu_logout -> Toast.makeText(this, "Clicou em Logout", Toast.LENGTH_SHORT).show()
-                R.id.menu_profile -> Toast.makeText(this, "Clicou em Profile", Toast.LENGTH_SHORT).show()
-                R.id.menu_share -> Toast.makeText(this, "Clicou em Share", Toast.LENGTH_SHORT).show()
-            }
-            return true
+        when (item?.itemId) {
+//            R.id.action_menu_profile -> Toast.makeText(this, "Clicou em perfil", Toast.LENGTH_SHORT).show()
+            R.id.action_menu_profile -> startActivity(Intent(this, ClienteProfile :: class.java))
         }
-        return super.onOptionsItemSelected(item)
+        return true
     }
-
 }
--- app/src/main/java/br/com/alexandre_salgueirinho/iquefome_kotlin/ClienteProfile.kt
+++ app/src/main/java/br/com/alexandre_salgueirinho/iquefome_kotlin/ClienteProfile.kt
@@ -2,11 +2,38 @@ package br.com.alexandre_salgueirinho.iquefome_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class ClienteProfile : AppCompatActivity() {
+
+    private lateinit var mToolbar: androidx.appcompat.widget.Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cliente_profile)
+
+        mToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(mToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_menu_profile_page, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_menu_profile_page_deslogar -> Toast.makeText(this, "Clicou em deslogar", Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
